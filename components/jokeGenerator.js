48418f26439f6becc02d62ffead2641d8df8726b
Eli Bradford <elibradford227@gmail.com>
2023-07-11 13:14:34 -0500

    getting joke from api

---
+++ components/jokeGenerator.js
@@ -0,0 +1,12 @@
+// // import React, { useState } from 'react';
+// // import PropTypes from 'prop-types';
+// // import getJoke from '../api/jokeData';
+
+export default function JokeGenerator() {
+  return (
+    <div>
+      <h2>Get The Punchline</h2>
+      <button type="button">Get It!</button>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,32 @@
+import React, { useState } from 'react';
+import getJoke from '../api/jokeData';
+
 function Home() {
+  // const handleClick = () => {
+  //   setEventText('Display joke here');
+  // };
+  // const [eventText, setEventText] = useState('');
+  // function renderSetup() {
+  //   const array = [];
+  //   getJoke().then((data) => {
+  //     array.push(data.setup);
+  //   }).then(console.warn(array));
+
+  //   setEventText(array);
+  // }
+  const [jokeSetup, setJokeSetup] = useState('');
+  const [jokeDelivery, setJokeDelivery] = useState('');
+
+  const handleJoke = async () => {
+    try {
+      const joke = await getJoke();
+      setJokeSetup(joke.setup);
+      setJokeDelivery(joke.delivery);
+    } catch (error) {
+      console.warn('Unable to retrieve joke');
+    }
+  };
+
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
@@ -9,7 +37,10 @@ function Home() {
         margin: '0 auto',
       }}
     >
-      <h1>Welcome Home!</h1>
+      <h1>Joke Generator!</h1>
+      <button type="button" onClick={handleJoke}>Get A Joke!</button>
+      <h3 className="watch-event">{jokeSetup || ''}</h3>
+      <h4 className="watch-event">{jokeDelivery || ''}</h4>
     </div>
   );
 }
